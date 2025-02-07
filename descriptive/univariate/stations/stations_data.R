cb23ce9a5e47a9a3effe4e7f1847c4daaa097439
Eduardo <eduardo.mantovani@ithinkupc.com>
2025-02-07 14:13:50 +0100

    stations analysis R

--- descriptive/univariate/stations/stations_data.R
+++ descriptive/univariate/stations/stations_data.R
@@ -11,7 +11,7 @@ con <- dbConnect(
 )
 
 # Fetch the station table
-stations_data <- dbGetQuery(con, "SELECT * FROM stations")
+stations_data <- dbGetQuery(con, "select station_id, lat, lon, altitude, address, cross_street, capacity, last_updated, geom from vw_latest_station_info;")
 
 # Save the data locally
 saveRDS(stations_data, "stations_data.rds")
